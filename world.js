fcdf9b96273e031a74ebb9d4fafff2b534cd6cd6
Dayna Clare <daycanimate@gmail.com>
2023-11-19 16:00:52 -0600

    Completed Exercise 5

--- world.js
+++ world.js
@@ -3,12 +3,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const rqst = new XMLHttpRequest();
     const lookupbtn = document.getElementById('lookup');
+    const lookupctybtn = document.getElementById('lookupcity');
     let countryfile = "world.php";
     let countryinput;
     let maindiv = document.getElementById('maindiv');
     let resultsdiv = document.getElementById('results');
     
     console.log(lookupbtn);    
+    console.log(lookupctybtn); 
     console.log(resultsdiv);
     console.log(maindiv);
     
@@ -40,6 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         resultsdiv.innerHTML = "";
 
                         resultsdiv.innerHTML = "Country not found. Please try again.";
+                    
                     } else {
                         resultsdiv.innerHTML = "";                     
                         
@@ -56,6 +59,52 @@ document.addEventListener('DOMContentLoaded', () => {
         };
         
         rqst.open('GET', countryfile+"?country="+countryinput);
+        rqst.send();
+    });
+
+    lookupctybtn.addEventListener('click', function(el){  
+        el.preventDefault();
+
+        countryinput = document.getElementById('country').value;  
+        
+        console.log(countryinput);       
+
+        rqst.onreadystatechange = function (){
+            if (rqst.readyState === XMLHttpRequest.DONE){
+                if (rqst.status === 200){
+                    let reply = rqst.responseText;                     
+                    console.log(reply);   
+
+                    if (reply.includes("All Countries")){
+                        resultsdiv.innerHTML = "";
+                        
+                        reply = reply.replaceAll('All Countries', '');
+                        console.log(reply);  
+
+                        resultsdiv.innerHTML = reply;
+                            
+                        console.log(reply);
+                        //alert(reply);
+
+                    } else if (reply.includes("<td>") == false){
+                        resultsdiv.innerHTML = "";
+
+                        resultsdiv.innerHTML = "Please type the full name of a Country to search for Cities. 'Lookup' can provide assistance.";
+                    
+                    } else {
+                        resultsdiv.innerHTML = "";                     
+                        
+                        resultsdiv.innerHTML = reply;
+                    }
+                    
+                                    
+                } else {
+                    alert('There was a problem with the request.')
+                }
+            }
+        };
+        
+        rqst.open('GET', countryfile+"?country="+countryinput+"&lookup=cities");
         rqst.send();
     });
    
--- world.php
+++ world.php
@@ -6,6 +6,12 @@ $dbname = 'world';
 
 $country = htmlspecialchars($_GET['country'], ENT_COMPAT);
 $country = strip_tags($country);
+$lookup;
+if (isset($_GET['lookup'])){
+  $lookup = htmlspecialchars($_GET['lookup'], ENT_COMPAT);
+  $lookup = strip_tags($lookup);
+}
+
 
 if (empty($country)) {  
   $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
@@ -25,11 +31,37 @@ if (empty($country)) {
     echo "<tr>" . "\n";
     echo  "<td>" . $row['name'] . "</td>";
     echo  "<td>" . $row['continent'] . "</td>";
-    echo  "<td>" . $row['independence_year'] . "</td>";
+    if ($row['independence_year'] === null){
+      echo "<td>" . "Not independent" . "</td>";
+    } else {
+      echo "<td>" . $row['independence_year'] . "</td>";
+    }  
     echo  "<td>" . $row['head_of_state'] . "</td>";
     echo "</tr>" . "\n";
   endforeach;
   echo "</table>";
+
+} elseif(isset($lookup)) {
+  if ($lookup === "cities"){
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $stmt = $conn->query("SELECT * FROM countries JOIN cities ON code=cities.country_code WHERE countries.name LIKE '$country'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<table>" . "\n";
+    echo "<tr>" . "\n";
+    echo "<th>City Name</th>" . "\n";
+    echo "<th>District</th>" . "\n";
+    echo "<th>Population</th>" . "\n";
+    echo "</tr>" . "\n";
+    foreach ($results as $row):
+      echo "<tr>" . "\n";
+      echo  "<td>" . $row['name'] . "</td>";
+      echo  "<td>" . $row['district'] . "</td>";
+      echo  "<td>" . $row['population'] . "</td>";
+      echo "</tr>" . "\n";
+    endforeach;
+    echo "</table>";
+  }  
 
 } else {
   $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
@@ -46,15 +78,18 @@ if (empty($country)) {
   echo "</tr>" . "\n";
   foreach ($results as $row):
     echo "<tr>" . "\n";
-    echo  "<td>" . $row['name'] . "</td>";
-    echo  "<td>" . $row['continent'] . "</td>";
-    echo  "<td>" . $row['independence_year'] . "</td>";
-    echo  "<td>" . $row['head_of_state'] . "</td>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "<td>" . $row['continent'] . "</td>";
+    if ($row['independence_year'] === null){
+      echo "<td>" . "Not independent" . "</td>";
+    } else {
+      echo "<td>" . $row['independence_year'] . "</td>";
+    }    
+    echo "<td>" . $row['head_of_state'] . "</td>";
     echo "</tr>" . "\n";
   endforeach;
   echo "</table>";
 }
-
 
 
 ?>
